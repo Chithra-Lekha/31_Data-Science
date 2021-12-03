ba73c267aa68549fb72c2439d37d08062c810e51
Chithra-Lekha <79584685+Chithra-Lekha@users.noreply.github.com>
2021-12-03 11:29:09 +0530

    program 5

---
+++ Matrix/n5.py
@@ -0,0 +1,11 @@
+import numpy as np
+m=np.array([[1,2,3,4,5], [6,7,8,9,10]])
+n=np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print("Original matrix:")
+print(m)
+print(n)
+print("matrix sum:",np.array(m)+np.array(n))
+print("matrix subtraction:",np.array(m)-np.array(n))
+print("matrix division:",np.array(m)/np.array(n))
+
+
